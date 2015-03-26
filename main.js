8d3c1962a850ec74b0af7a164c2595b448c88051
unknown <sfedorov@LAS-59WKBZ1-D.dpu.depaul.edu>
2015-03-26 12:01:27 -0500

    Fixed the display formatting and added a condition in case RMP doesnt have a professor on record

--- main.js
+++ main.js
@@ -2,6 +2,48 @@ function main() {
   setInterval(hello, 10000);  
 }   //end main()
 
+function CreatePopup(result, profName) {
+    if ($("body").find(".RMPDisplayArea").length != 0) {
+        $("body").find(".RMPDisplayArea").remove();
+    }
+
+    var parsedProfessor = [];
+    var hasData = true;
+
+    try {
+         parsedProfessor = JSON.parse(result);
+    } catch(err){
+        hasData = false;
+    }
+
+    //Load the template HTML file
+    $.get(chrome.extension.getURL("popup.html"), function(html) {
+
+        if (hasData == true) {
+
+            html = html.replace("__PROFESSOR_NAME__", profName);
+            html = html.replace("__OVERALL_QUALITY__", parsedProfessor.Grades[0].Rating);
+            html = html.replace("__AVERAGE_GRADE__", parsedProfessor.Grades[1].Rating);
+
+            html = html.replace("__HELPFULNESS__", parsedProfessor.Ratings[0].Rating);
+            html = html.replace("__CLARITY__", parsedProfessor.Ratings[1].Rating);
+            html = html.replace("__EASINESS__", parsedProfessor.Ratings[2].Rating);
+        } else {
+
+            var naString = "N/A";
+
+            html = html.replace("__PROFESSOR_NAME__", profName);
+            html = html.replace("__OVERALL_QUALITY__", naString);
+            html = html.replace("__AVERAGE_GRADE__", naString);
+
+            html = html.replace("__HELPFULNESS__", naString);
+            html = html.replace("__CLARITY__", naString);
+            html = html.replace("__EASINESS__", naString);
+        }
+        $("body").append(html);
+    });
+}
+
 function hello() {
     if (document.getElementById('ptifrmtgtframe') !== null)
     {
@@ -19,8 +61,10 @@ function hello() {
                 if (profName != 'Staff') {
                     $(professor).append("'<input class='ratingButton' type='button' value='SHOW RATING' />'").click(function () {
 
+                        var searchProfString = profName.split(" ").join("%20");
+
                         //Does jQuery really not have a param/arg string?
-                        var searchUrl = 'http://www.sergueifedorov.com/rmpapi/search/' + profName;
+                        var searchUrl = 'http://www.sergueifedorov.com/rmpapi/search/' + searchProfString;
                         console.log(searchUrl);
 
                         chrome.runtime.sendMessage({
@@ -31,40 +75,22 @@ function hello() {
 
                                 //Obtain the actual link to the page
                                 var searchResult = JSON.parse(response);
-                                var pageURL = searchResult[0].URL;
 
-                                var professorPageURL = 'http://www.sergueifedorov.com/rmpapi/Professor?url=' + pageURL;
+                                if (searchResult.length == 0) {
+                                    //We don't have any data from the web service
+                                    CreatePopup(searchResult, profName);
+                                } else {
+                                    var pageURL = searchResult[0].URL;
 
-                                chrome.runtime.sendMessage({
-                                    method: "GET",
-                                    url: professorPageURL
-                                }, function (result) {
+                                    var professorPageURL = 'http://www.sergueifedorov.com/rmpapi/Professor?url=' + pageURL;
 
-                                    var htmlToParse = "";
-
-                                    if ($("body").find(".RMPDisplayArea").length != 0) {
-                                        $("body").find(".RMPDisplayArea").remove();
-                                    }
-
-                                    var parsedProfessor = JSON.parse(result);
-                                        //Load the template HTML file
-                                        $.get(chrome.extension.getURL("popup.html"), function(html) {
-                                            htmlToParse = html;
-
-                                            html = html.replace("__PROFESSOR_NAME__", profName);
-                                            html = html.replace("__OVERALL_QUALITY__", parsedProfessor.Grades[0].Rating);
-                                            html = html.replace("__AVERAGE_GRADE__", parsedProfessor.Grades[1].Rating);
-
-                                            html = html.replace("__HELPFULNESS__", parsedProfessor.Ratings[0].Rating);
-                                            html = html.replace("__CLARITY__", parsedProfessor.Ratings[1].Rating);
-                                            html = html.replace("__EASINESS__", parsedProfessor.Ratings[2].Rating);
-
-                                            $("body").append(html);
-                                        });
-
-                                    
-
-                                });
+                                    chrome.runtime.sendMessage({
+                                        method: "GET",
+                                        url: professorPageURL
+                                    }, function(result) {
+                                        CreatePopup(result, profName);
+                                    });
+                                }
                             });
                     });
                 }
