0eb9bad1c723ea9d2d10d1a2109563083db5b2f7
SergueiFedorov <blackraven36@gmail.com>
2015-03-29 15:54:24 -0500

    Split the code files into multiple JS files. Fixed the bug where not all the links to the review page worked

--- main.js
+++ main.js
@@ -1,47 +1,27 @@
+//DISCLAIMRER: 
+//We, Ozer Chagatai and Serguei Fedorov are not responsible for third party modification, repackaging and redistribution of this source code. 
+//Please be aware that third party distribution of this extention may contain melicious source code which is beyond our control.
+
+//The source code of RateMyPaul, produced by Ozer Chagatai and Serguei Fedorov does not collect and will never collect student and faculty data protected by FERPA.
+//This extention only uses the names of DePaul faculty to produce search results. DePaul faculty names are publicly available both through a guest Campus Connect account
+//as well as the public facing DePaul Website.
+
+//The complete source code is available on: https://github.com/ochagata/rateMyPaul
+//The master branch contains the source code shipped with the extention
+
+//This file relies on:
+//LoadingAreaFunctionality.js
+//PopupFunctionality.js
+//Make sure they are imported
+
+
 (function() {
 
     //Entry point
     main();
 
-    //Sudo-Constant Vars (no such thing as constants in JavaScript)
-    var PROFESSOR_NAME_FIELD = "__PROFESSOR_NAME__";
-    var OVERALL_QUALITY_FIELD = "__OVERALL_QUALITY__";
-    var AVERAGE_GRADE_FIELD = "__AVERAGE_GRADE__";
-    var HELPFULNESS_FIELD = "__HELPFULNESS__";
-    var CLARITY_FIELD = "__CLARITY__";
-    var EASINESS_FIELD = "__EASINESS__";
-    var LINK_TO_PROFESSOR_PAGE = "__LINK_TO_PROFESSOR_PAGE__";
-    var CLOSE_BUTTON = "__CLOSE_BUTTON__";
-    var LINK_ICON = "__LINK_ICON__";
-
     function main() {
         setInterval(AugmentPage, 3000);
-    }
-
-    function CloseLoadingArea() {
-        if ($(".LoadingArea").length > 0) {
-            $(".LoadingArea").remove();
-        }
-    }
-
-    function CreateLoadingArea() {
-        if ($(".LoadingArea").length == 0) {
-            $.get(chrome.extension.getURL("loading.html"), function (html) {
-                $("Body").append(html);
-                $(".LoadingArea").append("<img src='" + chrome.extension.getURL('loading.GIF') + "'/>")
-            });
-        }
-    }
-
-    
-    function ClosePopup() {
-        if (PopupIsOpen()) {
-            $(".RMPDisplayArea").remove();
-        }
-    }
-
-    function PopupIsOpen() {
-        return $(".RMPDisplayArea").length > 0;
     }
 
     function ProfessorNameStillOnPage(professorName, documentToSearch)
@@ -58,59 +38,6 @@
         });
 
         return professorFound;
-    }
-
-    function CreatePopup(result, profName, pageUrl) {
-        
-        var parsedProfessor = null;
-        var hasData = true;
-
-        try {
-            parsedProfessor = JSON.parse(result);
-        } catch (err) {
-            hasData = false;
-        }
-
-        //Remove the loading animation if it's showing
-        ClosePopup();
-
-        //If the user keeps clicking, it will open multiple display areas on top of each other
-        CloseLoadingArea();
-
-        //Load the template HTML file
-        $.get(chrome.extension.getURL("popup.html"), function(html) {
-
-            html = html.replace(CLOSE_BUTTON, chrome.extension.getURL('close.png'));
-            html = html.replace(LINK_ICON, chrome.extension.getURL('link.png'));
-
-            if (hasData == true) {
-
-                html = html.replace(PROFESSOR_NAME_FIELD, profName);
-                html = html.replace(OVERALL_QUALITY_FIELD, parsedProfessor.Grades[0].Rating);
-                html = html.replace(AVERAGE_GRADE_FIELD, parsedProfessor.Grades[1].Rating);
-
-                html = html.replace(HELPFULNESS_FIELD, parsedProfessor.Ratings[0].Rating);
-                html = html.replace(CLARITY_FIELD, parsedProfessor.Ratings[1].Rating);
-                html = html.replace(EASINESS_FIELD, parsedProfessor.Ratings[2].Rating);
-
-                html = html.replace(LINK_TO_PROFESSOR_PAGE, "http://www.ratemyprofessors.com" + pageUrl);
-
-            } else {
-
-                var naString = "N/A";
-
-                html = html.replace(PROFESSOR_NAME_FIELD, profName);
-                html = html.replace(OVERALL_QUALITY_FIELD, naString);
-                html = html.replace(AVERAGE_GRADE_FIELD, naString);
-
-                html = html.replace(HELPFULNESS_FIELD, naString);
-                html = html.replace(CLARITY_FIELD, naString);
-                html = html.replace(EASINESS_FIELD, naString);
-
-                html = html.replace(LINK_TO_PROFESSOR_PAGE, "");
-            }
-            $("body").append(html);
-        });
     }
 
     function ProcessSearchResults(response, popupDelegate, professorName) {
